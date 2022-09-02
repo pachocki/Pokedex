82c4149b8ee459ad59ff907a102a5ffed2fda838
pachocki <101109218+pachocki@users.noreply.github.com>
2022-09-02 23:30:04 +0200

    updt

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const typeColor = {
 
 const url = " https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
-const btn=document.getElementById("btn")
+const btn = document.getElementById("btn")
 
 
 
